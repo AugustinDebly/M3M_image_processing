4342801dbf4cd2a6fecf2c2c2c4ab36e45273e6e
Augustin Debly <158299815+AugustinDebly@users.noreply.github.com>
2024-02-15 12:32:40 +0100

    Add files via upload

--- ECC_alignment_calibration.py
+++ ECC_alignment_calibration.py
@@ -4,21 +4,26 @@ import numpy as np
 
 #https://learnopencv.com/image-alignment-ecc-in-opencv-c-python/
 
+#Evangelidis, G. D., & Psarakis, E. Z. (2008). Parametric image alignment using enhanced correlation coefficient maximization. IEEE transactions on pattern analysis and machine intelligence, 30(10), 1858-1865.
+
 path_in_1  = sys.argv[1]
 path_in_2  = sys.argv[2]
 path_out   = sys.argv[3]
 
-img2TIF = cv2.imread(path_in_2,cv2.IMREAD_UNCHANGED)
-img1 = cv2.imread(path_in_1)
-img2 = cv2.imread(path_in_2)
+img2TIF  = cv2.imread(path_in_2,cv2.IMREAD_UNCHANGED)
+img1     = cv2.imread(path_in_1)
+img2     = cv2.imread(path_in_2)
 img1gray = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-sz = img1.shape
-warp_mode = cv2.MOTION_TRANSLATION
-warp_matrix = np.eye(2, 3, dtype=np.float32)
+
+sz                   = img1.shape
+warp_mode            = cv2.MOTION_TRANSLATION
+warp_matrix          = np.eye(2, 3, dtype=np.float32)
 number_of_iterations = 5000
-termination_eps = 1e-10
-criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations,  termination_eps)
+termination_eps      = 1e-10
+criteria             = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations,  termination_eps)
+
 (cc, warp_matrix) = cv2.findTransformECC(img1gray,img2gray,warp_matrix, warp_mode, criteria)
-img2_corr = cv2.warpAffine(img2TIF, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+img2_corr         = cv2.warpAffine(img2TIF, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+
 cv2.imwrite(path_out, img2_corr)
--- distorsion_calibration.py
+++ distorsion_calibration.py
@@ -4,19 +4,21 @@ import numpy as np
 
 path_in  = sys.argv[1]
 path_out = sys.argv[2]
-fx = float(sys.argv[3])
-fy = float(sys.argv[4])
-cx = float(sys.argv[5])
-cy = float(sys.argv[6])
-k1 = float(sys.argv[7])
-k2 = float(sys.argv[8])
-p1 = float(sys.argv[9])
-p2 = float(sys.argv[10])
-k3 = float(sys.argv[11])
+fx       = float(sys.argv[3])
+fy       = float(sys.argv[4])
+cx       = float(sys.argv[5])
+cy       = float(sys.argv[6])
+k1       = float(sys.argv[7])
+k2       = float(sys.argv[8])
+p1       = float(sys.argv[9])
+p2       = float(sys.argv[10])
+k3       = float(sys.argv[11])
 
 img = cv2.imread(path_in,cv2.IMREAD_UNCHANGED)
-camera_matrix = np.array([[fx, 0, cx],[0, fy, cy],[0, 0, 1]], dtype=np.float32)
-dist_coeff = np.array([k1, k2, p1, p2, k3], dtype=np.float32)
+
+camera_matrix      = np.array([[fx, 0, cx],[0, fy, cy],[0, 0, 1]], dtype=np.float32)
+dist_coeff         = np.array([k1, k2, p1, p2, k3], dtype=np.float32)
 newcameramatrix, _ = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coeff, (2592, 1944), 1, (2592, 1944))
-image_corrige = cv2.undistort(img, camera_matrix, dist_coeff, None, newcameramatrix)
+image_corrige      = cv2.undistort(img, camera_matrix, dist_coeff, None, newcameramatrix)
+
 cv2.imwrite(path_out, image_corrige)
